5bdc3929feae832f85db160d99bda2d2244ecf89
Gobind Preet Singh <gpsingh@timberlea.cs.dal.ca>
2024-07-17 21:30:54 -0300

    Fixed runtest.sh

--- prosim/runtest.sh
+++ prosim/runtest.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# USAGE: 
-# To run all the tests 
-#   ./runtest.sh 
+# USAGE:
+# To run all the tests
+#   ./runtest.sh
 # To run a single test, e.g., 13
 #   ./runtest.sh 13
 
-TESTS0="00 01 02 03 04"
-TESTS1="10 11 12 13 14 15 16 17 18 19"
-TESTS2="20 21 22 23 24 25 26 27 28 29"
-TESTS3="30 31 32 33 34 35 36 37 38 39"
-TESTS="$TESTS0 $TESTS1 $TESTS2 $TESTS3"
+TESTS0="00 01 02 03 04 05 06 07 08 09"
+TESTS="$TESTS0"
 EXE=prosim
 
 if [ -x $EXE ]; then
